6cc5a95390120c9eacabc1159ac263f94340066a
Vannda <vandakol222@gmail.com>
2021-03-22 23:48:34 +0700

    New branch

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center"></p>
+                <p className="text-center">Hello Moon</p>
             )}
             {items.map(item => (
                 <ItemDisplay
