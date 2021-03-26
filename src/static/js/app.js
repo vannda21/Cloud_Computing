02def3f972388ce17e7d3839b0eb47b0f214997f
Vannda <vandakol222@gmail.com>
2021-03-26 09:53:26 +0700

    test change

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Hello Moon</p>
+                <p className="text-center">Hello Moon and Sun</p>
             )}
             {items.map(item => (
                 <ItemDisplay
